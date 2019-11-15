d23299e9f3ebcbce46e10a4d61c56c14f48bdbb2
nwabuike <nwabuikeanthony@gmail.com>
2019-11-15 12:14:41 +0100

    Added Acitivities and Subject Controllers

---
+++ app/Http/Controllers/ActivitiesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ActivitiesController extends Controller
+{
+    //
+    public function CreateUserActivities()
+    {
+
+    }
+    public function getAllActivities()
+    {
+
+    }
+    public function getUserActivities($user_id)
+    {
+
+     }
+    public function getusersActivities($object_id)
+    {
+
+    }
+     public function editActivities()
+    {
+
+    }
+    public function deleteActivities()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/SubjectsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubjectsController extends Controller
+{
+    //
+    public function getAllSubject(){
+
+    }
+    public function getOneSubject($id)
+    {
+
+    }
+    public function editSubject($request)
+    {
+
+    }
+    public function deleteSubject($id)
+    {
+
+    }
+
+}
