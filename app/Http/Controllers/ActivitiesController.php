25fb978bc83f0b884a8eac820e85ea6c28978c21
nwabuike <nwabuikeanthony@gmail.com>
2019-11-17 13:14:21 +0100

    subject and activities commit update

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Activities;
 
 class ActivitiesController extends Controller
 {
@@ -13,22 +14,27 @@ class ActivitiesController extends Controller
     }
     public function getAllActivities()
     {
+        $activities = Activities::all();
 
     }
     public function getUserActivities($user_id)
     {
+        $activities = Activities::find($user_id);
 
      }
     public function getusersActivities($object_id)
     {
+        $activities = Activities::all($object_id);
 
     }
      public function editActivities()
     {
-
+        $subject = Activities::find();
     }
     public function deleteActivities()
     {
+        $activities = Activities::find();
+        $activities->delete();
 
     }
 }
