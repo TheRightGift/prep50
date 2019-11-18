62341e837c6e5659223ea21f49993d3a961555de
browny32 <ifydensofoe@gmail.com>
2019-11-18 09:52:21 +0100

    objective cntroller

---
+++ app/Http/Controllers/ObjectiveController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Objective;
+use Illuminate\Http\Request;
+
+class ObjectiveController extends Controller
+{
+    public function getAllObjective()
+    {
+
+
+    }
+
+    public function getOneObjective($id)
+    {
+
+        
+    }
+
+    public function getAllObjectiveInTopic($id)
+    {
+
+        
+    }
+    public function getAllObjectiveInSubject($id)
+    {
+
+        
+    }
+}
